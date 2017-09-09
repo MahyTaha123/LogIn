a7fceedb436522abc96ae65e8b9c6824acc5d657
mahytaha <mahy_taha@yahoo.com>
2017-09-09 20:31:17 +0200

    second commit

--- app/src/main/java/com/example/maha/login/MainActivity.java
+++ app/src/main/java/com/example/maha/login/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.maha.login;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button okk;
@@ -15,10 +18,21 @@ public class MainActivity extends AppCompatActivity {
     EditText phone;
     EditText email;
 
-    @Override
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            camera = (ImageView) findViewById(R.id.mImageView);
+
+
 
         ok = (Button) findViewById(R.id.ok);
         email = (EditText) findViewById(R.id.email);
@@ -26,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-       final String toMail= email.getText().toString();
+      //// final String toMail= email.getText().toString();
+
        final String email_Msg=emailMsg.getText().toString();
 
 
@@ -35,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String toMail= email.getText().toString();
                 Intent mailer = new Intent(Intent.ACTION_SEND);
                 mailer.setType("vnd.android.cursor.item/email");
                 mailer.putExtra(Intent.EXTRA_EMAIL, new String[]{toMail});
@@ -68,4 +84,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    ImageView camera ;
+
+    public void Camera(View v){
+
+
+        dispatchTakePictureIntent();
+
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            camera.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
